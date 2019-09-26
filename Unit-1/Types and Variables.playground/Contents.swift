6e3362268dd4c0a39a518aab73182670b5b93b3e
Kelby Mittan <kelbymittan@Kelbys-MacBook-Pro.local>
2019-09-26 10:43:44 -0400

    added playgrounds

---
+++ Unit-1/Types and Variables.playground/Contents.swift
@@ -0,0 +1,113 @@
+import UIKit
+
+//var str = "Hello, playground"
+
+//============================================================================
+//                          STRING
+//============================================================================
+
+var str = "Hello, Playground"
+
+// declared a fellow name
+// Type inference: implicit declaration of a variable
+var fellowName = "Kelby Mittan"
+
+// Type Annotation - explicity writing the data type using : (colon)
+var cohort: String = "iOS 6.3"
+
+//============================================================================
+//                         CHARACTER
+//============================================================================
+
+var alphabet: Character = "c"
+
+// mutating cohort
+// var is mutable - meaning can be changed
+cohort = "iOS 7.3"
+
+//============================================================================
+//                         INTEGER
+//============================================================================
+
+// declared variable age and initialized value of 32
+var ageOfFellow = 32
+
+var futureAge = ageOfFellow + 8
+
+futureAge
+
+print("Age of fellow 8 years from now is \(futureAge)")
+
+//============================================================================
+//                         DOUBLES
+//============================================================================
+
+var balance = 100005.9
+
+// re-assign balance a new value
+balance = balance + 85000
+
+print(balance)
+
+balance -= 180000
+
+print(balance)
+
+//============================================================================
+//                         Boolean - Bool, Comparison
+//============================================================================
+
+var result = 70 < 100
+
+print(result)
+
+var otherResult = result && true
+
+print(otherResult)
+
+-5 > -2 && 3 >= -5
+
+//============================================================================
+//                         Arithmetic operators
+//============================================================================
+
+//var salary = 20000
+//salary *= 2
+//print(salary)
+
+var salary = 20000
+salary *= 2
+print(salary)
+
+var marathonTime = 600
+marathonTime /= 2
+print(marathonTime)
+
+
+
+//============================================================================
+//                         Modulo operator or Remainder operator
+//============================================================================
+
+var modResult = 16 % 3
+print(modResult)
+
+var even = 12 % 6
+print(even)
+
+
+let numSeven = 4.0 + 1.2
+let numEight = 5.0 + 0.2
+let d = numSeven == numEight
+
+//let numThree = 24/5
+//let numFour = 24.0/5.0
+//let b = numThree == numFour
+
+let numOne = 4.0
+let numTwo = 4.0
+let a = numOne == numTwo
+
+let numFive = 24%5
+let numSix = 24.0%5.0
+let c = numFive == numSix
